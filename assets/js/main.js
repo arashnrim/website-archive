42417ddf41ab1bd8f08e001ee0349fb420c6fe1e
Arash <47273556+arashnrim@users.noreply.github.com>
2021-02-27 21:17:12 +0800

    🔥 Remove popup feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -192,48 +192,4 @@
     // Hide original img.
     $image_img.hide();
   });
-
-  // Poptrox.
-  $main.poptrox({
-    baseZIndex: 20000,
-    caption: function ($a) {
-      var s = "";
-
-      $a.nextAll().each(function () {
-        s += this.outerHTML;
-      });
-
-      return s;
-    },
-    fadeSpeed: 300,
-    onPopupClose: function () {
-      $body.removeClass("modal-active");
-    },
-    onPopupOpen: function () {
-      $body.addClass("modal-active");
-    },
-    overlayOpacity: 0,
-    popupCloserText: "",
-    popupHeight: 150,
-    popupLoaderText: "",
-    popupSpeed: 300,
-    popupWidth: 150,
-    selector: ".thumb > a.image",
-    usePopupCaption: true,
-    usePopupCloser: true,
-    usePopupDefaultStyling: false,
-    usePopupForceClose: true,
-    usePopupLoader: true,
-    usePopupNav: true,
-    windowMargin: 50,
-  });
-
-  // Hack: Set margins to 0 when 'xsmall' activates.
-  breakpoints.on("<=xsmall", function () {
-    $main[0]._poptrox.windowMargin = 0;
-  });
-
-  breakpoints.on(">xsmall", function () {
-    $main[0]._poptrox.windowMargin = 50;
-  });
 })(jQuery);
