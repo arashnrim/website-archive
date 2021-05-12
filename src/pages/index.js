7ee4086c1e3277ea3b72e1d4c53ff77696e46c2f
Arash <47273556+arashnrim@users.noreply.github.com>
2021-05-12 15:54:52 +0800

    📱 Improve footer responsive design

--- src/pages/index.js
+++ src/pages/index.js
@@ -234,31 +234,31 @@ const Introduction = () => (
     </div>
 
     {/* Footer */}
-    <div className="justify-center items-center flex flex-col space-y-5 h-screen-1/3 min-w-screen bg-gray-50 text-center rounded-t-3xl">
-      <p>
+    <div className="justify-center items-center flex flex-col space-y-5 lg:space-y-10 h-screen-1/3 min-w-screen bg-gray-50 text-center rounded-t-3xl">
+      <p className="text-sm sm:text-lg 2xl:text-xl">
         Made with <FontAwesomeIcon icon={faHeart} /> by Arash Nur Iman.
       </p>
       <ul className="space-x-5 font-mono">
-        <li className="inline text-sm">intro</li>
-        <li className="inline text-sm">
+        <li className="inline text-sm sm:text-lg 2xl:text-xl">intro</li>
+        <li className="inline text-sm sm:text-lg 2xl:text-xl">
           <a className="underline hover:no-underline">about</a>
         </li>
-        <li className="inline text-sm">
+        <li className="inline text-sm sm:text-lg 2xl:text-xl">
           <a className="underline hover:no-underline">skills</a>
         </li>
-        <li className="inline text-sm">
+        <li className="inline text-sm sm:text-lg 2xl:text-xl">
           <a className="underline hover:no-underline">works</a>
         </li>
       </ul>
       <div className="mt-5 space-x-6">
         <a href="https://github.com/arashnrim" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faGithub} size="lg" />
         </a>
         <a href="https://dev.to/arashnrim" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faDev} />
+          <FontAwesomeIcon icon={faDev} size="lg" />
         </a>
         <a href="mailto:hello@arashnrim.me">
-          <FontAwesomeIcon icon={faEnvelope} />
+          <FontAwesomeIcon icon={faEnvelope} size="lg" />
         </a>
       </div>
     </div>
