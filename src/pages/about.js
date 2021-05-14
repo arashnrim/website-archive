3be48114a54a9ed1d821a6a87d7b6316d9e9facd
Arash <47273556+arashnrim@users.noreply.github.com>
2021-05-14 15:22:17 +0800

    ✨ Add placeholder for other pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import Placeholder from "../components/Placeholder";
+import Footer from "../components/Footer";
+
+const About = () => (
+  <main>
+    <Placeholder />
+    <Footer />
+  </main>
+);
+
+export default About;
---
+++ src/pages/works.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import Placeholder from "../components/Placeholder";
+import Footer from "../components/Footer";
+
+const Works = () => (
+  <main>
+    <Placeholder />
+    <Footer />
+  </main>
+);
+
+export default Works;
