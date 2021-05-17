81e04784eb27e3ef273af38db92189981ee593c9
Arash <47273556+arashnrim@users.noreply.github.com>
2021-05-17 19:09:07 +0800

    🚑️ Add required Link reference

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "gatsby";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
