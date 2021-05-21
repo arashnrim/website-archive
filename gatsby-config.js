11a320b1a5a12b31cce33e58b88658b5a5215e95
Arash <47273556+arashnrim@users.noreply.github.com>
2021-05-21 21:47:41 +0800

    🎨 Run linter

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: "Portfolio",
   },
-  plugins: [`gatsby-plugin-image`, `gatsby-plugin-sharp`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+  ],
 };
