ecb5249b3c82c19efcf9dea97ce1352c42b81cea
Arash <47273556+arashnrim@users.noreply.github.com>
2021-05-29 19:39:43 +0800

    📱 Improve header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
       id="header"
       className="fixed flex z-50 p-5 items-center w-screen h-max lg:h-20 bg-black bg-opacity-2-5"
     >
-      <div className="flex w-1/2 md:flex-grow items-center space-x-6">
+      <div className="flex flex-grow items-center space-x-6">
         <span className="select-none">
           <span className="font-mono font-bold text-2xl">#</span>
           <span className="font-sans font-bold text-2xl">A</span>
