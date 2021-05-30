7d738e7d0321ce64a8011d3d266045c19ed61e04
Arash <47273556+arashnrim@users.noreply.github.com>
2021-05-30 10:06:49 +0800

    💄 Implement dark mode on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { faGithub, faDev } from "@fortawesome/free-brands-svg-icons";
 import { faHeart, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => (
-  <div className="justify-center items-center flex flex-col space-y-5 lg:space-y-10 h-screen-1/3 min-w-screen bg-gray-50 text-center rounded-t-3xl">
+  <div className="justify-center items-center flex flex-col space-y-5 lg:space-y-10 h-screen-1/3 min-w-screen bg-gray-900 text-center rounded-t-3xl">
     <p className="text-sm sm:text-lg 2xl:text-xl">
       Made with <FontAwesomeIcon icon={faHeart} /> by Arash Nur Iman.
     </p>
