dc883c550292da169db5aed0025f38aa7ae91f03
Arash <47273556+arashnrim@users.noreply.github.com>
2021-06-06 11:16:17 +0800

    💫 Add opacity animation on link hovers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,26 @@ const Footer = () => (
       Made with <FontAwesomeIcon icon={faHeart} /> by Arash Nur Iman.
     </p>
     <div className="mt-5 space-x-6">
-      <a href="https://github.com/arashnrim" target="_blank" rel="noreferrer">
+      <a
+        className="transition-all hover:opacity-75"
+        href="https://github.com/arashnrim"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FontAwesomeIcon icon={faGithub} size="lg" />
       </a>
-      <a href="https://dev.to/arashnrim" target="_blank" rel="noreferrer">
+      <a
+        className="transition-all hover:opacity-75"
+        href="https://dev.to/arashnrim"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FontAwesomeIcon icon={faDev} size="lg" />
       </a>
-      <a href="mailto:hello@arashnrim.me">
+      <a
+        className="transition-all hover:opacity-75"
+        href="mailto:hello@arashnrim.me"
+      >
         <FontAwesomeIcon icon={faEnvelope} size="lg" />
       </a>
     </div>
