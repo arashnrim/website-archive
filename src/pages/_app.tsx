cb37e75da88906c21263928256f947c115ab1e06
Arash <47273556+arashnrim@users.noreply.github.com>
2021-06-21 16:32:04 +0800

    💥 Adopt TypeScript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import "../styles/global.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { AppProps } from "next/app";
+import Head from "next/head";
 config.autoAddCss = false;
 
-import Head from "next/head";
-
-const MyApp = ({ Component, pageProps }) => (
+const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <link rel="icon" href="favicon.ico" />
