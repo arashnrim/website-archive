40fd06394bca1fe84a32a08efa053045e2cefb67
Arash <47273556+arashnrim@users.noreply.github.com>
2021-06-22 10:52:40 +0800

    🚑️ Fix TypeScript reversion issue

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,10 @@
 import "../styles/global.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
-import { AppProps } from "next/app";
 import Head from "next/head";
 config.autoAddCss = false;
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
+const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
       <link rel="icon" href="favicon.ico" />
