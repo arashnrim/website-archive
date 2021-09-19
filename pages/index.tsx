8036c19d710d9cc6b5e7905daa6819b385d6c12c
Arash <47273556+arashnrim@users.noreply.github.com>
2021-09-19 13:13:43 +0800

    🎨 Run linter

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,12 +60,12 @@ const Introduction = ({ repos }: IntroductionProps) => (
       <div className="justify-center items-center flex flex-col lg:flex-row space-y-6 md:space-y-6 lg:space-y-0 lg:space-x-16 min-h-screen min-w-screen bg-gray-900 rounded-b-3xl">
         <div className="w-1/2 max-w-sm bg-gray-900 ">
           <Image
-              className="pointer-events-none rounded-full"
-              src={profilePicture}
-              alt={"Arash Nur Iman"}
-              layout={"responsive"}
-              priority={true}
-              quality={60}
+            className="pointer-events-none rounded-full"
+            src={profilePicture}
+            alt={"Arash Nur Iman"}
+            layout={"responsive"}
+            priority={true}
+            quality={60}
           />
         </div>
         <div className="flex-row text-center lg:text-left">
