457950809828466389c8f72c2fc0ea4606d565a1
Arash <47273556+arashnrim@users.noreply.github.com>
2021-09-21 18:00:26 +0800

    💄 Improve Hero

--- components/Index/Hero.tsx
+++ components/Index/Hero.tsx
@@ -6,12 +6,12 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Hero = () => (
-  <div
+  <section
     className={
-      "justify-center items-center flex flex-col lg:flex-row space-y-6 md:space-y-6 lg:space-y-0 lg:space-x-16 min-h-screen min-w-screen bg-gray-900 rounded-b-3xl"
+      "flex flex-col lg:flex-row justify-center items-center min-h-screen min-w-screen px-10 lg:px-20 space-y-10 lg:space-y-0 lg:space-x-16 bg-black rounded-b-3xl"
     }
   >
-    <div className="w-1/2 max-w-sm bg-gray-900 ">
+    <div className="w-1/2 max-w-sm">
       <Image
         className="pointer-events-none rounded-full"
         src={profilePicture}
@@ -22,21 +22,21 @@ const Hero = () => (
       />
     </div>
     <div className="flex-row text-center lg:text-left">
-      <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-heading">
+      <p className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-heading">
         <span role="img" aria-label="Waving hand">
           👋
         </span>{" "}
         Hello, world!
       </p>
-      <h1 className="mt-2.5 text-4xl sm:text-6xl md:text-6xl 2xl:text-7xl font-heading font-semibold">
+      <h1 className="mt-2.5 text-4xl sm:text-6xl 2xl:text-7xl font-heading font-semibold">
         I&#39;m Arash Nur Iman.
       </h1>
-      <p className="mt-5 text-xl sm:text-2xl max-w-3xl">
+      <p className="mt-5 max-w-3xl sm:text-xl md:text-2xl ">
         I&#39;m a student developer from Singapore passionate about delivering
         simple, efficient, and easy-to-use creations of technology.
       </p>
 
-      <div className="mt-5 space-x-6">
+      <div className="mt-5 space-x-10">
         <a
           className="transition-all hover:opacity-75"
           href="https://github.com/arashnrim"
@@ -44,18 +44,18 @@ const Hero = () => (
           rel="noreferrer"
           aria-label="GitHub profile"
         >
-          <FontAwesomeIcon icon={faGithub} size="2x" />
+          <FontAwesomeIcon icon={faGithub} size="3x" />
         </a>
         <a
           className="transition-all hover:opacity-75"
           href="mailto:hello@arashnrim.me"
           aria-label="Email"
         >
-          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+          <FontAwesomeIcon icon={faEnvelope} size="3x" />
         </a>
       </div>
     </div>
-  </div>
+  </section>
 );
 
 export default Hero;
