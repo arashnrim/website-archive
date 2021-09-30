349418dab2c62881127e5a989c95e780bfc78602
Arash <47273556+arashnrim@users.noreply.github.com>
2021-09-30 10:15:07 +0800

    💄 Improve Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const Header = () => (
   <header className="fixed flex z-50 p-5 items-center w-screen h-max lg:h-20 bg-black bg-opacity-2-5 backdrop-filter backdrop-blur-sm">
-    <Link href="/">
-      <a className="bg-opacity-5 p-3 w-min">
+    <Link href="/" passHref={true}>
+      <span className="transition bg-opacity-5 p-3 w-min cursor-pointer hover:opacity-75">
         <span className="font-mono font-bold text-2xl">#</span>
         <span className="font-sans font-bold text-2xl">A</span>
-      </a>
+      </span>
     </Link>
   </header>
 );
