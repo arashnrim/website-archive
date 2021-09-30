610a08ea5b9ffc31466870d3eeea97b8f01fb860
Arash <47273556+arashnrim@users.noreply.github.com>
2021-09-30 10:15:19 +0800

    💄 Improve 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import Meta from "../components/Meta";
 import Layout from "../components/Layout";
+import InlineLink from "../components/InlineLink";
 
 const NotFound = () => {
   return (
@@ -11,16 +12,16 @@ const NotFound = () => {
       <Layout>
         <div className="justify-center items-center flex flex-col min-h-screen min-w-screen px-7 bg-gray-900">
           <p className="text-xl sm:text-2xl">Woah oh.</p>
-          <h1 className="text-9xl font-heading font-bold">404</h1>
+          <h1 className="text-9xl font-heading font-bold z-50">404</h1>
           <p className="lg:w-2/3 mt-5 text-xl sm:text-2xl text-center">
             Nothing was found at the requested page. For a single-page website,
             anyway, there shouldn&#39;t be anything else accessible but the root
             page!
           </p>
-          <Link href="/">
-            <a className="transition-all mt-5 font-bold underline hover:no-underline">
+          <Link href="/" passHref={true}>
+            <span className="mt-5 transition dotted text-xl sm:text-2xl hover:opacity-75 cursor-pointer">
               Need help going to the root page?
-            </a>
+            </span>
           </Link>
         </div>
       </Layout>
