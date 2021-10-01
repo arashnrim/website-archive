539bda54136af0f5060c243ad75fabbf7a0f5d7a
Arash <47273556+arashnrim@users.noreply.github.com>
2021-10-01 15:48:53 +0800

    💄 Improve 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,29 +3,40 @@ import Link from "next/link";
 
 import Meta from "../components/Meta";
 import Layout from "../components/Layout";
-import InlineLink from "../components/InlineLink";
+
+const teasers = [
+  "Uh oh.",
+  "Whoops...",
+  "Wait a minute...",
+  "Huh?",
+  "Oh, hey!",
+  "Something's not right...",
+];
 
 const NotFound = () => {
   return (
-    <main className="bg-gray-800 text-white">
+    <>
       <Meta />
       <Layout>
-        <div className="justify-center items-center flex flex-col min-h-screen min-w-screen px-7 bg-gray-900">
-          <p className="text-xl sm:text-2xl">Woah oh.</p>
-          <h1 className="text-9xl font-heading font-bold z-50">404</h1>
-          <p className="lg:w-2/3 mt-5 text-xl sm:text-2xl text-center">
+        <section className="justify-center items-center flex flex-col min-h-screen min-w-screen px-7 text-center text-xl sm:text-2xl bg-black">
+          <p>{teasers[Math.floor(Math.random() * teasers.length)]}</p>
+          <span className="my-10">
+            <h1 className="text-9xl font-heading font-bold">404</h1>
+            <p className="font-mono">Not Found</p>
+          </span>
+          <p className="lg:w-2/3">
             Nothing was found at the requested page. For a single-page website,
             anyway, there shouldn&#39;t be anything else accessible but the root
             page!
           </p>
           <Link href="/" passHref={true}>
-            <span className="mt-5 transition dotted text-xl sm:text-2xl hover:opacity-75 cursor-pointer">
+            <span className="mt-5 transition dotted hover:opacity-75 cursor-pointer">
               Need help going to the root page?
             </span>
           </Link>
-        </div>
+        </section>
       </Layout>
-    </main>
+    </>
   );
 };
 
