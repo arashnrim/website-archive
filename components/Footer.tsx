7073abcca8f85c972e393264e30cd6c150d4b9b5
Arash <47273556+arashnrim@users.noreply.github.com>
2021-10-02 11:18:29 +0800

    💄 Improve Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faHeart, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import Socials from "./Socials";
+import InlineLink from "./InlineLink";
 
 const Footer = () => (
-  <footer className="justify-center items-center flex flex-col space-y-5 lg:space-y-10 min-h-screen-1/3 min-w-screen bg-gray-900 text-center rounded-t-3xl">
-    <p className="text-sm sm:text-lg 2xl:text-xl">
-      Made with <FontAwesomeIcon icon={faHeart} /> by Arash Nur Iman.
+  <footer className="justify-center items-center flex flex-col space-y-5 md:space-y-10 min-h-screen-1/3 min-w-screen bg-black text-center rounded-t-3xl">
+    <p className="text-xl">
+      Made with <FontAwesomeIcon icon={faHeart} /> by Arash Nur Iman;{" "}
+      <InlineLink link="https://github.com/arashnrim/website" redirect={true}>
+        open-sourced on GitHub
+      </InlineLink>
+      .
     </p>
     <Socials />
   </footer>
