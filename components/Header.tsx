6580e52eab1351d8ea053fc4f44163ccbd9af102
Arash <47273556+arashnrim@users.noreply.github.com>
2021-12-11 18:11:49 +0800

    Hide Header when scrolling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,34 @@
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 const navLinks = ["about", "skills", "works"];
 
 const Header = () => {
+  const [y, setY] = useState(0);
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () =>
+      setTimeout(() => {
+        setHidden(window.scrollY !== y ? true : false);
+        setY(window.scrollY);
+      }, 1500);
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.clearTimeout(handleScroll());
+    };
+  }, [y]);
+
   return (
-    <header className="fixed flex z-50 p-5 items-center w-screen lg:h-20 bg-black bg-opacity-[0.025] backdrop-filter backdrop-blur-sm">
+    <motion.header
+      initial={{ opacity: 1 }}
+      animate={hidden ? { opacity: 0 } : { opacity: 1 }}
+      className="fixed flex z-50 p-5 items-center w-screen lg:h-20 bg-black bg-opacity-[0.025] backdrop-filter backdrop-blur-sm"
+    >
       <Link href="/" passHref={true}>
         <span className="p-3 transition-opacity duration-200 cursor-pointer w-min bg-opacity-5 hover:opacity-75">
           <span className="font-mono text-2xl font-bold">#</span>
@@ -20,7 +44,7 @@ const Header = () => {
           </Link>
         ))}
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
