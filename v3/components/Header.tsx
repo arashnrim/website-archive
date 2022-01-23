fb83795b87ffb911726eb55035ec36e6b26dff5a
Arash <47273556+arashnrim@users.noreply.github.com>
2022-01-23 09:20:15 +0800

    Refresh Header

--- v3/components/Header.tsx
+++ v3/components/Header.tsx
@@ -4,17 +4,24 @@ const navLinks = ["about", "skills", "works"];
 
 const Header = () => {
   return (
-    <header className="fixed z-50 flex items-center w-screen p-5 lg:h-20 bg-gradient-to-b from-black">
-      <Link href="/" passHref={true}>
-        <span className="p-3 transition-opacity  cursor-pointer w-min bg-opacity-5 hover:opacity-75">
-          <span className="font-mono text-2xl font-bold">#</span>
-          <span className="font-sans text-2xl font-bold">A</span>
+    <header className="fixed z-50 flex items-center lg:flex-col h-20 w-screen p-5 lg:h-screen lg:w-20 bg-gradient-to-b lg:bg-gradient-to-r from-black lg:space-y-6">
+      <Link href="/" passHref={true} scroll={true}>
+        <span className="p-3 lg:p-0 transition-opacity cursor-pointer group">
+          <span className="font-mono text-2xl font-bold group-hover:font-normal">
+            #
+          </span>
+          <span className="font-sans text-2xl font-extrabold group-hover:font-normal">
+            A
+          </span>
         </span>
       </Link>
-      <nav className="flex flex-row pl-5 space-x-5">
+      <nav className="flex-row lg:flex-col pl-5 lg:pl-0 space-x-5 lg:space-x-0 lg:space-y-12 flex lg:w-full justify-center">
         {navLinks.map((item) => (
           <Link href={`#${item}`} key={item} passHref>
-            <button type="button" className="sm:text-xl">
+            <button
+              type="button"
+              className="text-xl transition-all hover:font-extrabold lg:rotate-90"
+            >
               {item}
             </button>
           </Link>
