92b9f1adf37dcbff42ecc5af6078c770b48c1557
Arash <47273556+arashnrim@users.noreply.github.com>
2022-01-27 13:36:30 +0800

    Update global

--- v3/components/Section.tsx
+++ v3/components/Section.tsx
@@ -27,7 +27,7 @@ const Section = ({ children, id }: SectionProps) => {
   return (
     <motion.section
       className={
-        "flex flex-col justify-center items-center px-10 pt-20 space-y-10 h-auto min-h-screen text-center min-w-screen md:px-20 lg:px-36 md:text-left"
+        "flex flex-col justify-center items-center px-10 py-40 space-y-10 h-auto min-h-screen text-center min-w-screen md:px-20 lg:px-36 md:text-left"
       }
       id={id ? id : undefined}
       ref={reference}
