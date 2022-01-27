d18c4b82a51b3d710ebf2e9c917e8aec211bf2ff
Arash <47273556+arashnrim@users.noreply.github.com>
2022-01-27 13:11:56 +0800

    Update Header

--- v3/components/Header.tsx
+++ v3/components/Header.tsx
@@ -4,9 +4,9 @@ const navLinks = ["about", "skills", "works"];
 
 const Header = () => {
   return (
-    <header className="fixed z-50 flex items-center lg:flex-col h-20 w-screen p-5 lg:h-screen lg:w-20 bg-gradient-to-b lg:bg-gradient-to-r from-black lg:space-y-6">
+    <header className="fixed z-50 flex items-center w-screen h-20 p-5 lg:flex-col lg:h-screen lg:w-20 bg-gradient-to-b lg:bg-gradient-to-r from-black lg:space-y-6">
       <Link href="/" passHref={true} scroll={true}>
-        <span className="p-3 lg:p-0 transition-opacity cursor-pointer group">
+        <span className="p-3 transition-opacity cursor-pointer lg:p-0 group">
           <span className="font-mono text-2xl font-bold group-hover:font-normal">
             #
           </span>
@@ -15,12 +15,12 @@ const Header = () => {
           </span>
         </span>
       </Link>
-      <nav className="flex-row lg:flex-col pl-5 lg:pl-0 space-x-5 lg:space-x-0 lg:space-y-12 flex lg:w-full justify-center">
+      <nav className="flex flex-row justify-center pl-5 space-x-5 lg:flex-col lg:pl-0 lg:space-x-0 lg:space-y-8 lg:w-full">
         {navLinks.map((item) => (
           <Link href={`#${item}`} key={item} passHref>
             <button
               type="button"
-              className="text-xl transition-all hover:font-extrabold lg:rotate-90"
+              className="text-xl transition-all hover:font-extrabold lg:vertical-text"
             >
               {item}
             </button>
