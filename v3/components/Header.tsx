80322ba0ebadf6b490a438d1a48423a0256e8d6d
Arash <47273556+arashnrim@users.noreply.github.com>
2022-01-30 00:57:14 +0800

    Fix header

--- v3/components/Header.tsx
+++ v3/components/Header.tsx
@@ -8,9 +8,9 @@ const Header = () => {
   return (
     <header className="fixed z-50 flex items-center w-screen h-20 p-5 lg:flex-col lg:h-screen lg:w-20 bg-gradient-to-b lg:bg-gradient-to-r from-black lg:space-y-6">
       <Link href="/" passHref={true} scroll={true}>
-        <button className="h-8 w-8 group">
+        <button className="w-8 h-8 group">
           <Image
-            className="pointer-events-none rounded-lg group-hover:rounded-full transition-all"
+            className="transition-all rounded-lg pointer-events-none group-hover:rounded-full"
             src={profilePicture}
             alt="Arash Nur Iman"
             layout="responsive"
@@ -22,12 +22,9 @@ const Header = () => {
       <nav className="flex flex-row justify-center pl-5 space-x-5 lg:flex-col lg:pl-0 lg:space-x-0 lg:space-y-8 lg:w-full">
         {navLinks.map((item) => (
           <Link href={`#${item}`} key={item} passHref>
-            <button
-              type="button"
-              className="text-xl transition-all hover:font-extrabold lg:vertical-text"
-            >
+            <a className="text-xl underline-offset-0 lg:vertical-text">
               {item}
-            </button>
+            </a>
           </Link>
         ))}
       </nav>
