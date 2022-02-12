87614495b9f4690f5543113f038aca2d7c6fd802
Arash <47273556+arashnrim@users.noreply.github.com>
2022-02-12 17:33:56 +0800

    Adjust Section paddings

--- v3/components/Section.tsx
+++ v3/components/Section.tsx
@@ -9,7 +9,7 @@ const Section = ({ children, id }: SectionProps) => {
   return (
     <section
       className={
-        "flex flex-col justify-center items-center px-10 py-40 space-y-10 h-auto min-h-screen text-center min-w-screen md:px-20 lg:px-36 md:text-left"
+        "flex flex-col justify-center items-center px-10 py-32 space-y-10 h-auto min-h-screen text-center min-w-screen md:px-20 lg:px-36 md:text-left"
       }
       id={id && id}
     >
